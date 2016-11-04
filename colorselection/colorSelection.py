d9e04434d22bb918d8de1578ef1b056ead54e937
agrim <agrim.singh@gmail.com>
2016-11-04 15:58:35 +0800

    account for relevant masked lines

--- colorselection/colorSelection.py
+++ colorselection/colorSelection.py
@@ -4,13 +4,12 @@ import numpy as np
 
 # Read in the image and print out some stats
 image = (mpimg.imread('test.png')*255).astype('uint8')
-print('This image is: ',type(image), 
-         'with dimensions:', image.shape)
 
 # Grab the x and y size and make a copy of the image
 ysize = image.shape[0]
 xsize = image.shape[1]
 color_select = np.copy(image)
+line_image = np.copy(image)
 
 # color selection criteria
 red_threshold = 200
@@ -19,13 +18,42 @@ blue_threshold = 200
 
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
-# bitwise or with the "|" character to identify pixels below the thresholds
-thresholds = (image[:,:,0] < rgb_threshold[0]) \
-            | (image[:,:,1] < rgb_threshold[1]) \
-            | (image[:,:,2] < rgb_threshold[2])
-color_select[thresholds] = [0,0,0]
-plt.imshow(color_select)
-# Display the image                 
-plt.show()
-mpimg.imsave('test-after.png', color_select)
+# triangular mask with defined vertices (since image view is consistent)
 
+left_bottom = [0, 600]
+right_bottom = [975, 600]
+apex = [450, 310]
+
+# linear fit (y=Ax+B) to each of the three sides of the triangle (make line la)
+# np.polyfit returns the coefficients [A, B] of the fit
+fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+# "bitwise or" to mask pixels below the threshold
+color_thresholds = (image[:,:,0] < rgb_threshold[0]) | \
+                    (image[:,:,1] < rgb_threshold[1]) | \
+                    (image[:,:,2] < rgb_threshold[2])
+
+# now region inside the lines (isolation isolation)
+XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
+                    (YY > (XX*fit_right[0] + fit_right[1])) & \
+                    (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+                    
+# masking selection
+color_select[color_thresholds | ~region_thresholds] = [0, 0, 0]
+# color selection where both criteria match
+line_image[~color_thresholds & region_thresholds] = [255, 0, 0]
+
+# show image, region and color selections
+plt.imshow(image)
+x = [left_bottom[0], right_bottom[0], apex[0], left_bottom[0]]
+y = [left_bottom[1], right_bottom[1], apex[1], left_bottom[1]]
+plt.plot(x, y, 'b--', lw=4)
+# plt.imshow(color_select)
+plt.imshow(line_image)
+
+plt.show()
+
+mpimg.imsave('test-after-line.png', line_image)
